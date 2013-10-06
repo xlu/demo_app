939e3a943700171be9f94f7d7d70ed9356d642e3
Xiaoming Lu <xiaoming.lu.backup@gmail.com>
2013-10-06 00:32:21 -0700

    Make a basic User model (including secure passwords)

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 
 gem 'rails', '3.2.11'
 gem 'bootstrap-sass'
+gem 'bcrypt-ruby', '3.0.1'
 
 group :development do
   gem 'sqlite3', '1.3.8'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :name
+  attr_accessible :email, :name, :password, :password_confirmation
   has_many :microposts
+  validates :name, :presence => true, :length => { :maximum => 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, :presence => true, :format => { :with => VALID_EMAIL_REGEX }, :uniqueness => { :case_sensitive => false }
+  validates :password, length: { minimum: 6 }
+
+  before_save { self.email = email.downcase }
+
+  has_secure_password
 end
